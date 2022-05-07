2c6a26a367abdaa7f3df51f988dff08bac27c59b
Alejandro Vazquez del Mercado Gutiérrez <alejandro_vazquez98@hotmail.com>
2022-05-07 14:57:52 -0500

    Agregar íconos en barra de navegación

--- src/navigation/BottomTab.js
+++ src/navigation/BottomTab.js
@@ -3,17 +3,33 @@ import React from 'react';
 import HomeScreen from '../screens/HomeScreen';
 import ProfileScreen from '../screens/ProfileScreen';
 
+import Icon from 'react-native-vector-icons/Ionicons';
+
 const Tab = createBottomTabNavigator();
 
 export const BottomTab = () => {
     
     return (
         <Tab.Navigator
-            screenOptions = {{
-                tabBarActiveTintColor: 'red',
-                tabBarActiveBackgroundColor: 'grey',
-                tabBarInactiveBackgroundColor: 'orange',
-            }}
+            screenOptions = { ({ route }) => ({
+                // tabBarActiveTintColor: 'red',
+                // tabBarActiveBackgroundColor: 'grey',
+                // tabBarInactiveBackgroundColor: 'orange',
+                tabBarIcon: ({ focused, color, size }) => {
+
+                    let iconName = '';
+                    switch ( route.name ) {
+                        case 'Home':
+                            iconName = 'home-outline';
+                            break;
+                        case 'Profile':
+                            iconName = 'person-circle-outline';
+                            break;
+                    }
+
+                    return <Icon name = { iconName } size = { size } color = { color } />
+                }
+            })}
         >
             <Tab.Screen name = 'Home' component = { HomeScreen } />
             <Tab.Screen name = 'Profile' component = { ProfileScreen } />
